1568997738c52fc79750caae759a9f7cf613d201
mldn <365530751@qq.com>
2015-11-02 23:11:53 +0800

    in Hello.java file add two statment

--- Hello.java
+++ Hello.java
@@ -1,7 +1,10 @@
 public class Hello {
 	public static void main(String args[]) {
+		//System.out.printlin("Hello World!");
 		System.out.printlin("Hello World!");
 		System.out.printlin("Hello World!");
-		System.out.printlin("Hello World!");
+		System.out.printlin("sadd");
+		System.out.printlin("Hssssrld!");
+		System.out.printlin("2222orld!");
 	}
 }
